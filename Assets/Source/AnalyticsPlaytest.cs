d183c779714e670623124949a0286098197c5b24
forcepusher <bionitsoup@gmail.com>
2022-12-05 16:02:41 +0300

    Add more sample calls

--- Assets/Source/AnalyticsPlaytest.cs
+++ Assets/Source/AnalyticsPlaytest.cs
@@ -8,6 +8,20 @@ public class AnalyticsPlaytest : MonoBehaviour
     {
         GameAnalytics.NewProgressionEvent(GAProgressionStatus.Start, "level1");
         yield return new WaitForSecondsRealtime(1f);
-        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Complete, "level1");
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Fail, "level1");
+
+        GameAnalytics.NewResourceEvent(GAResourceFlowType.Source, "gold", 123, "loot", "chest");
+        GameAnalytics.NewResourceEvent(GAResourceFlowType.Sink, "gold", 42, "shop", "shotgun");
+
+        GameAnalytics.NewAdEvent(GAAdAction.Clicked, GAAdType.Video, "yandex", "");
+        GameAnalytics.NewAdEvent(GAAdAction.Show, GAAdType.Video, "yandex", "");
+        GameAnalytics.NewAdEvent(GAAdAction.FailedShow, GAAdType.Video, "yandex", "");
+        GameAnalytics.NewAdEvent(GAAdAction.RewardReceived, GAAdType.Video, "yandex", "");
+
+        GameAnalytics.NewAdEvent(GAAdAction.Show, GAAdType.Interstitial, "yandex", "");
+        GameAnalytics.NewAdEvent(GAAdAction.FailedShow, GAAdType.Interstitial, "yandex", "");
+
+        GameAnalytics.NewDesignEvent("reviveAdOffer");
+        GameAnalytics.NewDesignEvent("reviveAdClick");
     }
 }
